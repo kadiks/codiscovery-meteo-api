89ec8389101ea1c778b751c20800f65b58a4bb5f
Jénaïc Cambré <me@jenaiccambre.com>
2020-11-11 17:00:15 +0100

    add static proxy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const axios = require('axios');
+const { request } = require('gaxios');
 
 const { PORT =  3000 } = process.env;
 
@@ -10,10 +11,18 @@ const HOST = 'https://www.metaweather.com';
 const app = express();
 
 app.get('*', async (req, res) => {
-    console.log('req.url', req.url);
+    // console.log('req.url', req.url);
     if (req.url.startsWith('/api')) {
         const { data } = await axios.get(`${HOST}${req.url}`);
         res.json(data);
+        return;
+    }
+    if (req.url.startsWith('/static')) { 
+        // https://stackoverflow.com/a/64531817/185771
+        (await request({
+            url:`${HOST}${req.url}`,
+            responseType: 'stream'
+        })).data.pipe(res);
         return;
     }
     res.send('Your url should start with: /api');
